64d951c9f2f1bb2adc7f4323b649fbd9bfc4eb7c
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-04-07 22:35:25 +0100

    creating forms WIP

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
 
 class PostsNew extends Component {
 
   render() {
     return (
-      <div>Create Form</div>
+      <form>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title','categories','content']
+})(PostsNew);
