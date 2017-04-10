cc7c9f60e8360797e5a56ea1918337f8b04c1873
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-04-10 23:41:48 +0100

    navigate back to index page upon delete on posts_show.js

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
 
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
 
   onDeleteClick() {
-    this.props.deletePost(this.props.params.id);
+    this.props.deletePost(this.props.params.id)
+      .then(()=> {
+        // blog post is deleted, navigate the user to the index page.
+        this.context.router.push('/');
+      });
   }
 
   render() {
